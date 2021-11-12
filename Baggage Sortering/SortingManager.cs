a9d73695e0dfd156c7f0ad155be160e5d680de43
patr4586 <patr4586@ZBC-E-RO-23234>
2021-11-12 12:52:36 +0100

    commit

--- Baggage Sortering/SortingManager.cs
+++ Baggage Sortering/SortingManager.cs
@@ -25,32 +25,41 @@ namespace Baggage_Sortering
                 {
                     Monitor.Enter(locker);
                     for (int i = 0; i < gateSize; i++)
-                        for (int j = 0; j < gateSize; j++)
-                            if (terminal[j].LuggageBufferIsFull() || counter[i].CounterBelt.IsFull)
-                            {
-                                Monitor.Wait(locker);
-                                break;
-                            }
-                            else if (CanBeSorted(counter, i) && counter[i].CounterBelt.Luggage[0].Destination == terminal[j].Destination)
-                            {
-                                terminal[j].TransferLuggageToTerminal(counter[i].CounterBelt.Luggage[0]);
-                                terminal[j].TriggerOnLuggageTransfered(counter[i].CounterBelt.Luggage[0]);
-                                counter[i].CounterBelt.RemoveAt(0);
-                                //todo:: make so the luggage only can be transfered 10 seconds after it was checked in.
-                            }
+                        if (CanBeSorted(counter, i))
+                            for (int j = 0; j < gateSize; j++)
+                                if (terminal[j].LuggageBufferIsFull() || counter[i].CounterBelt.IsFull)
+                                {
+                                    Monitor.Wait(locker);
+                                    break;
+                                }
+                                else if (CanBeSorted(counter, i) && counter[i].CounterBelt.Luggage[0].Destination == terminal[j].Destination)
+                                {
+                                    terminal[j].TransferLuggageToTerminal(counter[i].CounterBelt.Luggage[0]);
+                                    terminal[j].TriggerOnLuggageTransfered(counter[i].CounterBelt.Luggage[0]);
+                                    counter[i].CounterBelt.RemoveAt(0);
+                                    break;
+                                    //todo:: make so the luggage only can be transfered 10 seconds after it was checked in.
+                                }
                 }
                 finally
                 {
                     Monitor.PulseAll(locker);
                     Monitor.Exit(locker);
                 }
+                Thread.Sleep(5000);
             }
         }
 
         private bool CanBeSorted(Counter[] counter, int index)
         {
             //FIX!!!!!!!
-            if (counter[index].CounterBelt.Luggage[0] != null && (counter[index].CounterBelt.Luggage[0].TimeStampIn - DateTime.UtcNow).Seconds > DateTime.UtcNow.Second + 5)
+            //if (counter[index].CounterBelt.Luggage[0] != null)
+            //{
+
+            //    Console.WriteLine("luggage: " + counter[index].CounterBelt.Luggage[0].TimeStampIn.AddSeconds(5).Second);
+            //    Console.WriteLine("time: " + DateTime.Now.Second);
+            //}
+            if (counter[index].CounterBelt.Luggage[0] != null)
                 return true;
             return false;
         }
